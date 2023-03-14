9991eb2b1577a7e39761eec8ce4d7adc5940b354
Mauka <213009edu@gmsil.com>
2023-03-14 15:19:05 +0600

    jekljsld

---
+++ src/modules/businessCard/api/businessCard.api.ts
@@ -0,0 +1,6 @@
+import api from '../../../common/api';
+import { BusinessCard } from '../../../common/interfaces/Business';
+
+const businessCardApi = api.injectEndpoints({
+	endpoints: (build) => ({}),
+});
