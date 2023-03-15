71849a58cb94f25d5117b424d205352e911f34aa
baltabaeva <baltabaeva005@mail.ru>
2023-03-15 16:07:42 +0600

    Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from '@mui/material';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Router from './app/components/Router';
 import { darkTheme } from './app/config/theme';
 import store from './app/store/store';
 import './styles/style.scss';
 import { Layout } from './ui';
+import CreateEventPage from "./pages/CreateEventPage";
+
 
 export default function App() {
 	return (
@@ -14,9 +16,13 @@ export default function App() {
 				<Layout>
 					<BrowserRouter>
 						<Router />
+						<Routes>
+							<Route path="/" element={<CreateEventPage/>}/>
+						</Routes>
 					</BrowserRouter>
 				</Layout>
 			</ThemeProvider>
 		</Provider>
+
 	);
 }
